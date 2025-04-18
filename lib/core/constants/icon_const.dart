03b63d8700670ecaa615a28074cf773184808a6f
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-18 08:31:58 +0600

    feat: add success icon SVG and update icon constants

--- lib/core/constants/icon_const.dart
+++ lib/core/constants/icon_const.dart
@@ -6,4 +6,5 @@ class IconConst {
   static const String facebookIcon = '${iconPath}facebook_icon.svg';
   static const String instaIcon = '${iconPath}insta_icon.svg';
   static const String logoIcon = '${iconPath}logo_icon.svg';
+  static const String successIcon = '${iconPath}success_icon.svg';
 }
