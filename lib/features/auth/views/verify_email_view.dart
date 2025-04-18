eb1904cdbae99d4a04fcdacb9450563de6b04896
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-18 08:12:04 +0600

    feat: implement VerifyEmailView with UI components for email verification

--- lib/features/auth/views/verify_email_view.dart
+++ lib/features/auth/views/verify_email_view.dart
@@ -1,13 +1,55 @@
+import 'package:aromaqueen/core/components/custom_appbar.dart';
+import 'package:aromaqueen/core/components/custom_button.dart';
+import 'package:aromaqueen/core/components/custom_input.dart';
+import 'package:aromaqueen/core/routes/app_pages.dart';
+import 'package:aromaqueen/core/themes/color_constants.dart';
+import 'package:aromaqueen/core/wrapper/padding_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class VerifyEmailView extends StatelessWidget {
   const VerifyEmailView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Verify Email Screen'),
+    return Scaffold(
+      appBar: customAppbar1(title: "Forget Password"),
+      body: PaddingWrapper(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(),
+            Column(
+              spacing: 24.h,
+              children: [
+                Text(
+                  "Select which contact details should we use to reset your password.",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.sp,
+                    color: ColorConstants.darkGray,
+                  ),
+                ),
+                CustomInputField(
+                  controller: TextEditingController(),
+                  title: "Email",
+                  isEmail: true,
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 40.h),
+              child: CustomButton(
+                btnTxt: "Continue",
+                onPressed: () {
+                  Get.toNamed(Routes.otpVerify);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
