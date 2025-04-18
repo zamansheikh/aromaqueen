2da09f719f6be79ea38fbb33850f1df2c2f2bea1
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-18 08:31:41 +0600

    NewPasswordView with UI components for password creation

--- lib/features/auth/views/new_password_view.dart
+++ lib/features/auth/views/new_password_view.dart
@@ -1,10 +1,61 @@
+import 'package:aromaqueen/core/components/custom_appbar.dart';
+import 'package:aromaqueen/core/components/custom_button.dart';
+import 'package:aromaqueen/core/components/custom_input.dart';
+import 'package:aromaqueen/core/themes/color_constants.dart';
+import 'package:aromaqueen/core/wrapper/padding_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NewPasswordView extends StatelessWidget {
   const NewPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('New Password View')));
+    return Scaffold(
+      appBar: customAppbar1(title: "Forget Password"),
+      body: PaddingWrapper(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(),
+            Column(
+              spacing: 24.h,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Create Your New password",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.sp,
+                    color: ColorConstants.darkGray,
+                  ),
+                ),
+                CustomInputField(
+                  controller: TextEditingController(),
+                  title: "New Password",
+                  isPassword: true,
+                ),
+                CustomInputField(
+                  controller: TextEditingController(),
+                  title: "Confirm New Password",
+                  isPassword: true,
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 40.h),
+              child: CustomButton(
+                btnTxt: "Continue",
+                onPressed: () {
+                
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
