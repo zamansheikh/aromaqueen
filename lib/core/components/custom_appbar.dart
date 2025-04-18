03192741cc77399bea9004150aa2dd55bc77b7b2
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-18 08:11:50 +0600

    feat: enhance customAppbar1 to accept a title parameter and improve leading icon functionality

--- lib/core/components/custom_appbar.dart
+++ lib/core/components/custom_appbar.dart
@@ -1,7 +1,25 @@
+import 'package:aromaqueen/core/themes/color_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-AppBar customAppbar1() {
+AppBar customAppbar1({required title}) {
   return AppBar(
-    leading: IconButton(onPressed: onPressed, icon: Icon(Icons.chevron_left)),
+    leading: IconButton(
+      onPressed: () {
+        Get.back();
+      },
+      icon: Icon(Icons.chevron_left, color: ColorConstants.dark, size: 24.sp),
+    ),
+    backgroundColor: ColorConstants.white,
+    title: Text(
+      title,
+      style: GoogleFonts.roboto(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w500,
+        color: ColorConstants.dark,
+      ),
+    ),
   );
 }
