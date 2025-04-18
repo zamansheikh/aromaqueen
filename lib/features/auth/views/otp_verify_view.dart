1e26fea21e84e0e36c5f91d573a8fcfe210d3143
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-18 08:24:02 +0600

    feat: implement OtpVerifyView with UI components for OTP verification and add pinput dependency for otp input.

--- lib/features/auth/views/otp_verify_view.dart
+++ lib/features/auth/views/otp_verify_view.dart
@@ -1,13 +1,92 @@
+import 'package:aromaqueen/core/components/custom_appbar.dart';
+import 'package:aromaqueen/core/components/custom_button.dart';
+import 'package:aromaqueen/core/components/custom_input.dart';
+import 'package:aromaqueen/core/routes/app_pages.dart';
+import 'package:aromaqueen/core/themes/color_constants.dart';
+import 'package:aromaqueen/core/wrapper/padding_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpVerifyView extends StatelessWidget {
   const OtpVerifyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('OtpVerifyView'),
+    return Scaffold(
+      appBar: customAppbar1(title: "Forget Password"),
+      body: PaddingWrapper(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(),
+            Column(
+              spacing: 24.h,
+              children: [
+                Text(
+                  "Code has been send to infogmaila@gmail.com",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15.sp,
+                    color: ColorConstants.darkGray,
+                  ),
+                ),
+                Pinput(
+                  controller: TextEditingController(),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  defaultPinTheme: PinTheme(
+                    width: 56.w,
+                    height: 42.h,
+                    textStyle: GoogleFonts.roboto(
+                      fontSize: 20.sp,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: ColorConstants.lightGray),
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Resend Code In",
+                      style: GoogleFonts.roboto(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w400,
+                        color: ColorConstants.gray,
+                      ),
+                    ),
+                    Text(
+                      " 53 s",
+                      style: GoogleFonts.roboto(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                        color: ColorConstants.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 40.h),
+              child: CustomButton(
+                btnTxt: "Continue",
+                onPressed: () {
+                  Get.toNamed(Routes.newPassword);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
